fc13bdeab68582fb2ce6c66bf5431ba1c00f9d5b
shanu17 <suk117@pitt.edu>
2020-11-19 00:19:36 -0500

    Creating schema

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const port = 3000;
 const path = require("path");
 const mysql = require("mysql");
 const db = require("./dbConnection");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -12,15 +11,24 @@ db.connect((err, result) => {
 		console.log(err);
 	else {
 		console.log("Connected to MySQL");
-		var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(100), password VARCHAR(255))";
-		db.query(sql, (err, result) => {
-			if(err)
-				console.log(err);
-			else
-				console.log(result);
-		});
+		var sql = [];
+		sql.push("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL)");
+		sql.push("CREATE TABLE seller (id INT AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, category VARCHAR(255) NOT NULL, CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE)");
+		sql.push("CREATE TABLE customer (id INT AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, profile_img VARCHAR(255), CONSTRAINT fk_users_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE)");
+		sql.push("CREATE TABLE menu (id INT AUTO_INCREMENT PRIMARY KEY, rest_id INT NOT NULL, title VARCHAR(255) NOT NULL, summary VARCHAR(255), CONSTRAINT fk_rest_id FOREIGN KEY (rest_id) REFERENCES seller (id) ON DELETE CASCADE)");
+		sql.push("CREATE TABLE orders (id INT AUTO_INCREMENT PRIMARY KEY, customer_id INT NOT NULL, rest_id INT NOT NULL, status BOOLEAN NOT NULL, total DECIMAL(7, 3) NOT NULL, CONSTRAINT fk_seller_id FOREIGN KEY (rest_id) REFERENCES seller (id) ON DELETE CASCADE, CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer (id))");
+		sql.push("CREATE TABLE items (id INT AUTO_INCREMENT PRIMARY KEY, menu_id INT NOT NULL, name VARCHAR(255) NOT NULL, summary VARCHAR(255), type VARCHAR(255) NOT NULL, price DECIMAL(7, 3) NOT NULL, CONSTRAINT fk_menu_id FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE)");
+		sql.push("CREATE TABLE order_items (id INT AUTO_INCREMENT PRIMARY KEY, order_id INT NOT NULL, quantity INT NOT NULL, item_id INT NOT NULL, CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE, CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES items (id))");
+		for (var i = 0; i < sql.length; i++) {
+			db.query(sql[i], (err, result) => {
+				if(err)
+					console.log("Already created table");
+				else
+					console.log(result);
+			});
+		}
 	}
-})
+});
 
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
@@ -30,7 +38,6 @@ app.use(express.static(publicDirectory));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cookieParser());
 
 module.exports = db;
 
